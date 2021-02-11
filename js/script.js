d6874fb30b6a85d57e956200a3ec0c849bc22643
velikiiadmin <73746565+velikiiadmin@users.noreply.github.com>
2021-02-11 10:24:27 +0200

    11.02.2021

--- js/script.js
+++ js/script.js
@@ -2,5 +2,14 @@ $(document).ready(function(){
     $('#profile__ripple').ripples({
         resolution: 512,
         dropRadius: 10
-    })
+    });
+
+    const bars = document.querySelectorAll(".progress__bar");
+    bars.forEach(function(bar){
+        let persentage = bar.dataset.percent;
+        let tooltip = bar.children[0];
+        tooltip.innerText = persentage + '%';
+        bar.style.width = persentage + '%';
+        console.log(persentage);
+    });
 });
